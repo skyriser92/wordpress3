a1dacf3a30e6bc8bb645e57aa64bd839f2c02dbb
Logan <lh252@njit.edu>
2019-03-01 14:16:13 -0500

    Task: Completed step 6 in the theme tut.

--- wp-content/themes/BootSTheme/index.php
+++ wp-content/themes/BootSTheme/index.php
@@ -1,2 +1,10 @@
 <?php get_header(); ?>
 <?php get_footer(); ?>
+<?php
+query_posts('posts_per_page=1');
+while(have_posts()) : the_post(); ?>
+    <div>
+        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <p><?php the_excerpt(); ?></p>
+    </div>
+<?php endwhile; wp_reset_query(); ?>
