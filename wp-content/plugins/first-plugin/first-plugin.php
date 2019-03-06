db5f46d61c86ef2470bcb5fc498c2378ebc8bb7b
skyriser92 <jcm44@njit.edu>
2019-03-06 12:23:01 -0500

    first plugin first commit

---
+++ wp-content/plugins/first-plugin/first-plugin.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jcjc9
+ * Date: 3/6/2019
+ * Time: 12:20 PM
+ * Plugin Name: Very first plugin
+ * Plugin URI: local/wordpress3
+ * Description: Project 1 of IS373 class
+ * Author: John McMinn
+ */
